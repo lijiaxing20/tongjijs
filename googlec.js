060596d8ce0bc645cc3f35525a47eaa12dbd022c
lijiaxing20 <65068894+lijiaxing20@users.noreply.github.com>
2025-03-03 09:47:47 +0800

    Update googlec.js

--- googlec.js
+++ googlec.js
@@ -1,9 +1,9 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-XHN3L6B9FW"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-XHN3L6B9FW');
-</script>
+document.writeln("<!-- Google tag (gtag.js) -->");
+document.writeln("<script async src=\'https://www.googletagmanager.com/gtag/js?id=G-XHN3L6B9FW\'></script>");
+document.writeln("<script>");
+document.writeln("  window.dataLayer = window.dataLayer || [];");
+document.writeln("  function gtag(){dataLayer.push(arguments);}");
+document.writeln("  gtag(\'js\', new Date());");
+document.writeln("");
+document.writeln("  gtag(\'config\', \'G-XHN3L6B9FW\');");
+document.writeln("</script>");
